3fbbca2e1b6a5e1c143a4c71beecc466caed1afc
SubkhankulovaK <125728607+SubkhankulovaK@users.noreply.github.com>
2023-02-26 16:17:08 +0300

    Update Main.java

--- src/main/java/ru/ck105/Main.java
+++ src/main/java/ru/ck105/Main.java
@@ -4,5 +4,5 @@ public class Main {
     privite static String msg = "Hello guru!!!!!!!"
     public static void main(String[] args) {
         System.out.println(msg);
-    }
+    }// комментарий
 }
