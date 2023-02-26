b4556fc81d05915e019fc8c0b6232f801a7826fb
SubkhankulovaK <125728607+SubkhankulovaK@users.noreply.github.com>
2023-02-26 16:39:22 +0300

    Update Main.java

--- src/main/java/ru/ck105/Main.java
+++ src/main/java/ru/ck105/Main.java
@@ -1,8 +1,12 @@
 package ru.ck105;
 
+package guru.qa;
+
 public class Main {
-    privite static String msg = "Hello guru!!!!!!!"
-    public static void main(String[] args) {
-        System.out.println(msg);
-    }// комментарий
+  
+  private static String msg = "Hello qa.guru!";
+  
+  public static void main(String... args) {
+    System.out.println(msg);
+  }
 }
