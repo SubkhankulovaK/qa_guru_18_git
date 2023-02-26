88a84d80d05fcc9d8b65497ae14b1e0b9ab1b1af
SubkhankulovaK <ck.105@mail.ru>
2023-02-26 16:56:35 +0300

    added getMessage() method

--- src/main/java/ru/ck105/Main.java
+++ src/main/java/ru/ck105/Main.java
@@ -1,13 +1,11 @@
 package ru.ck105;
 
 public class Main {
-    privite static String msg = "Hello guru!!!!!!!"
-
     public static void main(String[] args) {
         System.out.println(getMessage());
     }
 
     private static String getMessage() {
-        return msg;
+        return "Hello qa.guru";
     }
 }
