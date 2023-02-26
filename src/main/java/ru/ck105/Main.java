6428cefd4023e7998626ea88e66b3462210f26fe
SubkhankulovaK <125728607+SubkhankulovaK@users.noreply.github.com>
2023-02-26 15:50:35 +0300

    Update Main.java

--- src/main/java/ru/ck105/Main.java
+++ src/main/java/ru/ck105/Main.java
@@ -1,7 +1,8 @@
 package ru.ck105;
 
 public class Main {
+    privite static String msg = "Hello guru!!!!!!!"
     public static void main(String[] args) {
-        System.out.println("Hello guru!!!!!!!");
+        System.out.println(msg);
     }
 }
